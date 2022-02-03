34aefb1a0c29c8ccb287def5c6d6082d3f60ddc0
jacob <jacoberichardson7@gmail.com>
2022-02-03 13:46:00 -0700

    git

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -2,6 +2,35 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
 router.use('/api', apiRouter);
 
 module.exports = router;
