e383cb377d7fd9941ebe822075446e7fc6e1728d
jacob <jacoberichardson7@gmail.com>
2022-02-03 11:29:06 -0700

    git

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -1,4 +1,42 @@
-const express = require('express')
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
+
+router.post(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login({ credential, password });
+
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user
+    });
+  })
+);
+
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6IkRlbW8tbGl0aW9uIn0sImlhdCI6MTY0MzkxMjQ4MSwiZXhwIjoxNjQ0NTE3MjgxfQ.PMikg36CiuP2iJhtZqTR4pyD1bSDFl3-yQW44Cr3STA
 
 module.exports = router;
