18e0ffeeb922348800ea9d74117cc800eb073a0d
jacob <jacoberichardson7@gmail.com>
2022-02-09 09:22:12 -0700

    finished seeder data

--- backend/db/seeders/20220208200608-comments.js
+++ backend/db/seeders/20220208200608-comments.js
@@ -14,6 +14,7 @@ module.exports = {
       {user_id: 3, song_id: 1, body: "Love it!", createdAt: new Date(), updatedAt: new Date() },
       {user_id: 3, song_id: 2, body: "Bro! What polysynth did you use to make this?", createdAt: new Date(), updatedAt: new Date() },
       {user_id: 3, song_id: 4, body: "Folk metal is the only real music, which makes this half a song", createdAt: new Date(), updatedAt: new Date() },
+      {user_id: 1, song_id: 5, body: "EPIC!!", createdAt: new Date(), updatedAt: new Date() },
       {user_id: 2, song_id: 7, body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", createdAt: new Date(), updatedAt: new Date() },
       {user_id: 3, song_id: 8, body: "PrEtTy GoOd, PrEtTy GoOd", createdAt: new Date(), updatedAt: new Date() },
       {user_id: 1, song_id: 9, body: "Wow, this is the only song I like besides my own. Thanks for sharing", createdAt: new Date(), updatedAt: new Date() },
