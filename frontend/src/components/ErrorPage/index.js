0ead53b50b5ca4941a0f29eafac7b17c82e246e8
jacob <jacoberichardson7@gmail.com>
2022-02-11 10:42:56 -0700

    error page looks great. Styling complete

--- frontend/src/components/ErrorPage/index.js
+++ frontend/src/components/ErrorPage/index.js
@@ -3,7 +3,11 @@ import './ErrorPage.css';
 
 function ErrorPage() {
   return (
-    <h1>test</h1>
+    <div className="error-page-main-div">
+      <img className="error-image" src="https://a-v2.sndcdn.com/assets/images/404-339e640d.png" />
+      <h2 className="error-title">We can't find that user.</h2>
+      <div className="error-body-text">A report has been sent to our tech team, and they're looking into the problem. Please check back in a bit.</div>
+    </div>
   )
 }
 
