db2963e5a38831471d49967666237815317138a5
jacob <jacoberichardson7@gmail.com>
2022-02-11 10:43:26 -0700

    small logical change to text

--- frontend/src/components/ErrorPage/index.js
+++ frontend/src/components/ErrorPage/index.js
@@ -5,7 +5,7 @@ function ErrorPage() {
   return (
     <div className="error-page-main-div">
       <img className="error-image" src="https://a-v2.sndcdn.com/assets/images/404-339e640d.png" />
-      <h2 className="error-title">We can't find that user.</h2>
+      <h2 className="error-title">We can't find that page.</h2>
       <div className="error-body-text">A report has been sent to our tech team, and they're looking into the problem. Please check back in a bit.</div>
     </div>
   )
